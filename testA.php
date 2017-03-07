116be2f90a2156fbe2f3594b71f18d09fdcc817f
comscisau-nitipong <s5752410029@sau.ac.th>
2017-03-07 14:30:59 +0700

    update #1 naja

---
+++ testA.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	qweqweqweqweqwe
+	qw
+	e
+	wqe
+	qw
+	e
+	qwe
+	q
+	we
+	wqw
+	eqqqqqq
+	qweqqqqq
+</body>
+</html>
